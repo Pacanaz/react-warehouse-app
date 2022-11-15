a4227b37cfa1e2eea62a8abb0e065e303953ae25
Tautvydas <tautvydas.gudziunas@gmail.com>
2022-11-15 22:16:49 +0200

    modal to add items

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,20 @@ import {
   Heading,
   theme,
   Spacer,
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  Input,
-  FormHelperText,
 } from "@chakra-ui/react"
-import { useForm } from "react-hook-form"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Flex textAlign="center" fontSize="xl" flexDirection='column' alignItems={'center'}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-          <Heading mt={'5%'}>
-            Warehouse App
-          </Heading>
-  <Spacer mb={'20vh'}></Spacer>
-         <ItemPage />
-         <FormControl>
-  <FormLabel>Email address</FormLabel>
-  <Input type='email' />
-  <FormHelperText>We'll never share your email.</FormHelperText>
-</FormControl>
-      
+      <ColorModeSwitcher justifySelf="flex-end" />
+      <Heading mt={'5%'}>
+        📦 Warehouse App
+      </Heading>
+      <Spacer mb={'20vh'}></Spacer>
+      <ItemPage />
+
+
     </Flex>
   </ChakraProvider>
 )
